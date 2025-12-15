251eb8a486dcdfbdd7f54cb0d53edcc3eb60751b
Sammy <myallio94@gmail.com>
2025-12-15 11:06:41 +1100

    remove ver from docker-compose

---
+++ backend/models/user.py
@@ -0,0 +1,43 @@
+from . import db
+from datetime import datetime
+
+
+class User(db.Model):
+    """User model for auth and user data"""
+
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # user auth fields
+    username = db.Column(db.String(50), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(255), unique=True, nullable=False, index=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    # other fields
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    watchlist = db.relationship(
+        "Watchlist", 
+        back_populates="user",
+        cascade="all, delete-orphan",
+        lazy="dynamic" #returns query object instead of list - can filter
+        )
+    
+    history = db.relationship(
+        "History",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        lazy="dynamic"
+    )
+    
+    ratings = db.relationship (
+        "Rating",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        lazy="dynamic" 
+    )
+
+    def __repr__(self):
+        '''displays user as username not object'''
+        return f"<User {self.username}>"
