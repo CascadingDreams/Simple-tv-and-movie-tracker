ef1458dc3ec38790c1584951e564b02bf9376843
Sammy <myallio94@gmail.com>
2026-02-02 20:01:44 +1100

    Fixed init

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -10,4 +10,26 @@ def init_db(app):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from . import user, media, genre, watchlist, history, rating
+    from . import associations
+    from . import user, media, genre, watchlist, history, rating, people
+
+    from .user import User
+    from .media import Media
+    from .genre import Genre
+    from .watchlist import Watchlist
+    from .history import History
+    from .rating import Rating
+    from .people import Person
+
+    __all__ = [
+        "db",
+        "migrate",
+        "init_db",
+        "User",
+        "Media",
+        "Genre",
+        "Watchlist",
+        "History",
+        "Rating",
+        "Person",
+    ]
